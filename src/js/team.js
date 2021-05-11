5f79babecec5082599313f164af42222232c593f
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-05-12 00:03:14 +0400

    files changed

--- src/js/team.js
+++ src/js/team.js
@@ -5,52 +5,54 @@
 /* eslint-disable no-sequences */
 export default class Team {
   constructor() {
-    this.magician = {
-      name: 'Magician',
-      type: 'Magician',
-      health: 100,
-      level: 1,
-      attack: 100,
-      defence: 40,
-    },
-    this.daemon = {
-      name: 'Daemon',
-      type: 'Daemon',
-      health: 100,
-      level: 1,
-      attack: 100,
-      defence: 40,
-    },
-    this.bowman = {
-      name: 'Bowman',
-      type: 'Bowman',
-      health: 0,
-      level: 1,
-      attack: 25,
-      defence: 25,
-    },
-    this.swordsman = {
-      name: 'Swordsman',
-      type: 'Swordsman',
-      health: -5,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    },
-    this.undead = {
-      name: 'Undead',
-      type: 'Undead',
-      health: 100,
-      level: 1,
-      attack: 25,
-      defence: 25,
-    };
+    this.members = [
+      {
+        name: 'Magician',
+        type: 'Magician',
+        health: 100,
+        level: 1,
+        attack: 100,
+        defence: 40,
+      },
+      {
+        name: 'Daemon',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        attack: 100,
+        defence: 40,
+      },
+      {
+        name: 'Bowman',
+        type: 'Bowman',
+        health: 0,
+        level: 1,
+        attack: 25,
+        defence: 25,
+      },
+      {
+        name: 'Swordsman',
+        type: 'Swordsman',
+        health: -5,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Undead',
+        type: 'Undead',
+        health: 100,
+        level: 1,
+        attack: 25,
+        defence: 25,
+      },
+    ];
   }
 
   * [Symbol.iterator]() {
-    const persons = Object.values(this);
-    for (let currentPersonIndex = 0; currentPersonIndex < persons.length; persons[currentPersonIndex++]) {
-      yield persons[currentPersonIndex];
+    const { members } = this;
+    for (let currentPersonIndex = 0; currentPersonIndex < members.length; members[currentPersonIndex++]) {
+      yield members[currentPersonIndex];
     }
   }
 }
